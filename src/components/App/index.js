cc55d83d1e1d2a880e89eca597046e07f9bdf93b
Celine Depauw <celinefauchier@hotmail.com>
2021-03-22 15:20:43 +0100

    creation of components and definition of the fonts and the colors - alex jb celine

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,15 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <header>Header de la page</header>
+    <div>Page</div>
+    <div>Navigation bas de page pour mobile</div>
+    <footer>Footer</footer>
   </div>
 );
 
