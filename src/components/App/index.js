b07d5c49c57db12e705159f4d85bcb804b3c69fd
celinedepauw <72380154+celinedepauw@users.noreply.github.com>
2021-03-22 18:08:41 +0100

    Revert "Home"

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,17 +2,14 @@
 import React from 'react';
 
 // == Import
-import Header from 'src/components/Header';
-import './styles.scss';
-
+import reactLogo from './react-logo.svg';
+import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Header />
-    <div>Page</div>
-    <div>Navigation bas de page pour mobile</div>
-    <footer>Footer</footer>
+    <img src={reactLogo} alt="react logo" />
+    <h1>Composant : App</h1>
   </div>
 );
 
