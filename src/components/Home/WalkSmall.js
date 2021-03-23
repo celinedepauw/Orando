d931b3bd91545b5bd5a4f2064efa188a815e7ea1
Celine Depauw <celinefauchier@hotmail.com>
2021-03-23 11:22:02 +0100

    component WalkSmall static with style

---
+++ src/components/Home/WalkSmall.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WalkSmall = () => (
+  <div className="home_walk_small">
+    <h3 className="home_walk_title">Titre de la rando</h3>
+    <p className="home_walk_date">Date de la rando</p>
+    <p className="home_walk_area">Région de la rando</p>
+    <p className="home_walk_difficulty">Difficulté de la rando</p>
+  </div>
+);
+
+export default WalkSmall;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import WalkSmall from 'src/components/Home/WalkSmall';
+
 import './home.scss';
 
 const Home = () => (
   <main className="home">
-    <h2 className="home_title">Explorez toutes les randonnées disponibles autour de chez vous !</h2>
+    <h2 className="home_title">Explorez toutes les randonnées disponibles !</h2>
     <div className="home_walks_list">
-      <div className="home_walk_small">1ere randonnée</div>
-      <div className="home_walk_small">2ème randonnée</div>
-      <div className="home_walk_small">3ème randonnée</div>
+      <WalkSmall />
+      <WalkSmall />
+      <WalkSmall />
+      <WalkSmall />
+      <WalkSmall />
+      <WalkSmall />
     </div>
     <div className="home_resume">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque autem tempore nemo veritatis incidunt, repellendus cumque iusto non porro voluptatum corporis sapiente maiores magni rem! Vel eveniet numquam odit atque?
