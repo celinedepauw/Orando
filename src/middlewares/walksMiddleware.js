d743ec2f8dbbe23a2c20fcd7c002615e287f8db7
Celine Depauw <celinefauchier@hotmail.com>
2021-03-23 14:37:13 +0100

    creation of walksMiddleware

---
+++ src/middlewares/walksMiddleware.js
@@ -0,0 +1,8 @@
+const walksMiddleware = (store) => (next) => (action) => {
+  console.log('on a intercepté une action dans le middleware: ', action);
+  switch (action.type) {
+    default:
+      next(action);
+  }
+};
+export default walksMiddleware;
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import walksMiddleware from 'src/middlewares/walksMiddleware';
 
 import walkReducer from 'src/reducers/walkReducer';
 
+const enhancers = composeWithDevTools(
+  applyMiddleware(
+    walksMiddleware,
+  ),
+);
 const store = createStore(
   // reducer
   walkReducer,
   // enhancer
-  devToolsEnhancer(),
+  enhancers,
 );
 
 export default store;
