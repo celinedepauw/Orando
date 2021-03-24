afc936cd3ee94ebd2bc21908424793ff8695ac3c
Alexandrine Heutte <alexandrine.h@gmail.com>
2021-03-24 13:12:56 +0100

    account page layout done

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,19 +2,31 @@ import React from 'react';
 
 import './account.scss';
 
+// preparation to use Link in the next step
+// import { Link } from 'react-router-dom';
+/*
+<Link
+    to={}
+    className="account_profil"
+    >
+      Fonctionnement du site
+</Link>
+*/
+
+
 // this component will be only static
 const Account = () => (
   <div className="account">
     <h2 className="account_title">Mon compte</h2>
     <div className="account_container">
       <div className="account_profil">
-        Mon profil
+        <a href="">Mon profil</a>
       </div>
       <div className="account_incomingwalks">
-        Mes randonnées à venir
+        <a href="">Mes randonnées à venir</a>
       </div>
       <div className="account_myhikes">
-        Mes randonnées organisées
+        <a href="">Mes randonnées organisées</a>
       </div>
     </div>
   </div>
