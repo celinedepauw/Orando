1cb1993de182211cd0c6e0b52997d3e99127ad9e
Celine Depauw <celinefauchier@hotmail.com>
2021-03-25 19:39:44 +0100

    add Link in NavTop

--- src/components/Header/NavTop.js
+++ src/components/Header/NavTop.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { BiHomeHeart, BiPlus } from 'react-icons/bi';
 import { FaHiking } from 'react-icons/fa';
@@ -6,10 +7,10 @@ import { IoMdPerson } from 'react-icons/io';
 
 const NavTop = () => (
   <nav className="header_nav">
-    <a href="" className="header_nav_item"><BiHomeHeart size={25} />Accueil</a>
-    <a href="" className="header_nav_item"><FaHiking size={25} /><BiPlus size={25} />Créer une randonnée</a>
-    <a href="" className="header_nav_item"><IoMdPerson size={25} />Mon Compte</a>
-    <a href="" className="header_nav_item"><IoMdPerson size={25} />Déconnexion</a>
+    <Link to="/" className="header_nav_item"><BiHomeHeart size={25} />Accueil</Link>
+    <Link to="/créer_une_randonnée" className="header_nav_item"><FaHiking size={25} /><BiPlus size={25} />Créer une randonnée</Link>
+    <Link to="/profile" className="header_nav_item"><IoMdPerson size={25} />Mon Compte</Link>
+    <Link to="/déconnexion" className="header_nav_item"><IoMdPerson size={25} />Déconnexion</Link>
   </nav>
 );
 
