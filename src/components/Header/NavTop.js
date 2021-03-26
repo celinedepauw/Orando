749469d91fb275c3328c88790bce44c7f36ddffd
Celine Depauw <celinefauchier@hotmail.com>
2021-03-26 13:09:18 +0100

    add link to the page walk/create from symfony

--- src/components/Header/NavTop.js
+++ src/components/Header/NavTop.js
@@ -8,7 +8,7 @@ import { IoMdPerson } from 'react-icons/io';
 const NavTop = () => (
   <nav className="header_nav">
     <Link to="/" className="header_nav_item"><BiHomeHeart size={25} />Accueil</Link>
-    <Link to="/create_hiking" className="header_nav_item"><FaHiking size={25} /><BiPlus size={25} />Créer une randonnée</Link>
+    <a href="http://orando.me/back/walk/create" className="header_nav_item"><FaHiking size={25} /><BiPlus size={25} />Créer une randonnée</a>
     <Link to="/account" className="header_nav_item"><IoMdPerson size={25} />Mon Compte</Link>
     <Link to="/disconnect" className="header_nav_item"><IoMdPerson size={25} />Déconnexion</Link>
   </nav>
