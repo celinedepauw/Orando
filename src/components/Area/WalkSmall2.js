90748d1b885d2d357ecace6e9237a1b415a6abae
Celine Depauw <celinefauchier@hotmail.com>
2021-03-28 12:36:55 +0200

    add Link to the detail of a walk

--- src/components/Area/WalkSmall2.js
+++ src/components/Area/WalkSmall2.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const WalkSmall2 = ({
+  id,
   title,
   date,
   difficulty,
@@ -12,11 +14,12 @@ const WalkSmall2 = ({
     <p className="area_walk_date">{date}</p>
     <p className="area_walk_difficulty">{difficulty}</p>
     <p className="area_walk_duration">{duration} heure(s)</p>
-    <a href="" className="area_walk_more_infos">En savoir plus </a>
+    <Link to={`/walks/${id}`} className="area_walk_more_infos">En savoir plus </Link>
   </div>
 );
 
 WalkSmall2.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   difficulty: PropTypes.string.isRequired,
