6453df08343c0c4b46085582d1035279477f631b
Alexandrine Heutte <alexandrine.h@gmail.com>
2021-03-30 12:08:14 +0200

    try to secure private page wip

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Home2 from 'src/containers/Home2';
 import Walk from 'src/containers/Walk';
@@ -11,38 +11,47 @@ import WalksToCome from 'src/containers/WalksToCome';
 import MyHikes from 'src/containers/MyHikes';
 import Authentification from 'src/containers/Authentification';
 
-const Page = () => (
-  <div className="page">
-    <Switch>
-      <Route path="/walks/:id">
-        <Walk />
-      </Route>
-      <Route path="/areas/:id">
-        <Area />
-      </Route>
-      <Route path="/Authentification">
-        <Authentification />
-      </Route>
-      <Route path="/account">
-        <Account />
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/incoming_walks">
-        <WalksToCome />
-      </Route>
-      <Route path="/my_hikes">
-        <MyHikes />
-      </Route>
-      <Route path="/">
-        <Home2 />
-      </Route>
-    </Switch>
-  </div>
-);
+const Page = ({ isLogged }) => {
+
+  const AuthRoute = props => {
+    const { isLogged, type} = props;
+    if (type === 'guest' && isLogged) return <Redirect to="/home" />;
+    if (type === 'private' && !isLogged) return <Redirect to="/" />;
+    return <Route {...props} />;
+  };
+  return(
+    <div className="page">
+      <Switch>
+        <Route path="/walks/:id">
+          <Walk />
+        </Route>
+        <Route path="/areas/:id">
+          <Area />
+        </Route>
+        <Route path="/Authentification">
+          <Authentification />
+        </Route>
+        <AuthRoute path="/account" type="private">
+          <Account />
+        </AuthRoute>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/incoming_walks">
+          <WalksToCome />
+        </Route>
+        <Route path="/my_hikes">
+          <MyHikes />
+        </Route>
+        <Route path="/">
+          <Home2 />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default Page;
--- src/containers/Page/index.js
+++ src/containers/Page/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { connect } from 'react-redux';
-
+import { Redirect, Route } from "react-router";
 import Page from 'src/components/Page';
+
+
 
 const mapStateToProps = (state) => ({
   isLogged: state.userInfo.isLogged,
