f202fa2268d06182a4daecb29cb64d1438336747
Alexandrine Heutte <alexandrine.h@gmail.com>
2021-03-30 13:25:33 +0200

    tests security about private route wip next step merge this branch in order to my colleagues can use authentification and after i will continue research

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -11,47 +11,39 @@ import WalksToCome from 'src/containers/WalksToCome';
 import MyHikes from 'src/containers/MyHikes';
 import Authentification from 'src/containers/Authentification';
 
-const Page = ({ isLogged }) => {
+const Page = () => (
+  <div className="page">
+    <Switch>
+      <Route path="/walks/:id">
+        <Walk />
+      </Route>
+      <Route path="/areas/:id">
+        <Area />
+      </Route>
+      <Route path="/Authentification">
+        <Authentification />
+      </Route>
+      <Route path="/account">
+        <Account />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/incoming_walks">
+        <WalksToCome />
+      </Route>
+      <Route path="/my_hikes">
+        <MyHikes />
+      </Route>
+      <Route path="/">
+        <Home2 />
+      </Route>
+    </Switch>
+  </div>
+);
 
-  const AuthRoute = props => {
-    const { isLogged, type} = props;
-    if (type === 'guest' && isLogged) return <Redirect to="/home" />;
-    if (type === 'private' && !isLogged) return <Redirect to="/" />;
-    return <Route {...props} />;
-  };
-  return(
-    <div className="page">
-      <Switch>
-        <Route path="/walks/:id">
-          <Walk />
-        </Route>
-        <Route path="/areas/:id">
-          <Area />
-        </Route>
-        <Route path="/Authentification">
-          <Authentification />
-        </Route>
-        <AuthRoute path="/account" type="private">
-          <Account />
-        </AuthRoute>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/incoming_walks">
-          <WalksToCome />
-        </Route>
-        <Route path="/my_hikes">
-          <MyHikes />
-        </Route>
-        <Route path="/">
-          <Home2 />
-        </Route>
-      </Switch>
-    </div>
-  );
-};
 
 export default Page;
--- src/containers/Page/index.js
+++ src/containers/Page/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect, Route } from "react-router";
+
 import Page from 'src/components/Page';
 
 
