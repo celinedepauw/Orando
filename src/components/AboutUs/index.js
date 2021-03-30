a4ce138d1b91c490fd61039d13b15347649dfba9
Jean-Baptiste Lafont <jeanbaptiste.lafont13@gmail.com>
2021-03-30 14:13:17 +0200

    new update page aboutUs

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -8,9 +8,25 @@ const AboutUs = () => (
   <div className="about_us">
     <h2 className="about_us_title">Qui sommes-nous?</h2>
     <img className="about_us_image" src={photo} alt="" />
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Nisi autem dolorem numquam hic maiores non dolor unde, nobis natus perferendis, 
+      eligendi, adipisci libero expedita dolores? Consectetur necessitatibus ut delectus voluptas?
+		Quidem laudantium porro voluptates consectetur iure possimus tempore nam voluptatum sit facilis temporibus, cumque perferendis suscipit.</p>
     <img className="about_us_image" src={photo} alt="" />
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Nisi autem dolorem numquam hic maiores non dolor unde, nobis natus perferendis, 
+      eligendi, adipisci libero expedita dolores? Consectetur necessitatibus ut delectus voluptas?
+		Quidem laudantium porro voluptates consectetur iure possimus tempore nam voluptatum sit facilis temporibus, cumque perferendis suscipit.</p>
     <img className="about_us_image" src={photo} alt="" />
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Nisi autem dolorem numquam hic maiores non dolor unde, nobis natus perferendis, 
+      eligendi, adipisci libero expedita dolores? Consectetur necessitatibus ut delectus voluptas?
+		Quidem laudantium porro voluptates consectetur iure possimus tempore nam voluptatum sit facilis temporibus, cumque perferendis suscipit.</p>
     <img className="about_us_image" src={photo} alt="" />
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Nisi autem dolorem numquam hic maiores non dolor unde, nobis natus perferendis, 
+      eligendi, adipisci libero expedita dolores? Consectetur necessitatibus ut delectus voluptas?
+		Quidem laudantium porro voluptates consectetur iure possimus tempore nam voluptatum sit facilis temporibus, cumque perferendis suscipit.</p>
     <img className="about_us_image" src={photo} alt="" />
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
       Nisi autem dolorem numquam hic maiores non dolor unde, nobis natus perferendis, 
