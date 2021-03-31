bab288156abaf5c89b74780dac49022d9cf63bd5
Celine Depauw <celinefauchier@hotmail.com>
2021-03-31 11:26:44 +0200

    wip

--- src/reducers/walkReducer.js
+++ src/reducers/walkReducer.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { SAVE_WALKS, SAVE_DELETE_WALK } from 'src/actions/walks';
-=======
-import { SAVE_WALKS } from 'src/actions/walks';
->>>>>>> delete an organized walk ok but need a refresh to not see it anymore
 
 const initialState = {
   walks: [],
@@ -18,14 +14,6 @@ function walkReducer(state = initialState, action) {
         walks: action.walks,
         loading: false,
       };
-<<<<<<< HEAD
-    case SAVE_DELETE_WALK:
-      return {
-        ...state,
-        walkId: action.walkId,
-      };
-=======
->>>>>>> delete an organized walk ok but need a refresh to not see it anymore
     default:
       return state;
   }
