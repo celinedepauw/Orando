f4778583c1100b9dc402d546a6781fdce69f39b3
Celine Depauw <celinefauchier@hotmail.com>
2021-04-02 17:00:37 +0200

    add profile picture

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -19,12 +19,15 @@ const Profile = ({ user, loadingUser }) => {
           </Link>
           <h2 className="profile_title">Mon Profil</h2>
           <div className="profile_container">
+            <div>
+              <img className="profile_picture" alt="avatar" src={`https://orando.me/o/images/users/${userDatas.picture}`} />
+            </div>
             <div className="profile_pseudo">Pseudo: {userDatas.nickname}</div>
             <div className="profile_fistname">Prénom: {userDatas.firstname}</div>
             <div className="profile_lastname">Nom: {userDatas.lastname}</div>
             <div className="profile_date_of_birth">Date de naissance: {userDatas.dateOfBirth}</div>
             <div className="profile_email">Email: {userDatas.email}</div>
-            <div className="profile_area">Région: besoin de la donnée provenant du back</div>
+            <div className="profile_area">Région: {userDatas.area}</div>
             <div className="profile_description">
               Description: {userDatas.description}
             </div>
