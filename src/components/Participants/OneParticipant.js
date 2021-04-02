8f7d9d2884ae8c33cd5125cdb719efae49a05bdc
Celine Depauw <celinefauchier@hotmail.com>
2021-04-02 12:57:13 +0200

    add a link to send an email to a participant

--- src/components/Participants/OneParticipant.js
+++ src/components/Participants/OneParticipant.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 import photo from 'src/assets/images/Moi.jpg';
 
-const OneParticipant = ({ nickname }) => (
+const OneParticipant = ({ id, nickname }) => (
   <div className="oneParticipant">
     <img className="one_participant_image" size={50} src={photo} alt="" />
-    <a className="one_participant_contact" href="">Contacter {nickname}</a>
+    <a href={`https://orando.me/o/profile/${id}/contact-user`} className="one_participant_contact">Contacter {nickname}</a>
   </div>
 );
 
 OneParticipant.propTypes = {
+  id: PropTypes.number.isRequired,
   nickname: PropTypes.string.isRequired,
 };
 
