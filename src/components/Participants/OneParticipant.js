f98ce6296b22c696c47a839e40e49fec3e5a09e1
Celine Depauw <celinefauchier@hotmail.com>
2021-04-02 12:52:03 +0200

    walk's title and date dynamic in Participants

--- src/components/Participants/OneParticipant.js
+++ src/components/Participants/OneParticipant.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import photo from 'src/assets/images/Moi.jpg';
 
-const OneParticipant = ({ nickname }) => {
-  console.log('résultat');
-  return (
-    <div className="oneParticipant">
-      <img className="one_participant_image" size={50} src={photo} alt="" />
-      <a className="one_participant_contact" href="">Contacter {nickname}</a>
-    </div>
+const OneParticipant = ({ nickname }) => (
+  <div className="oneParticipant">
+    <img className="one_participant_image" size={50} src={photo} alt="" />
+    <a className="one_participant_contact" href="">Contacter {nickname}</a>
+  </div>
 );
+
+OneParticipant.propTypes = {
+  nickname: PropTypes.string.isRequired,
 };
 
 export default OneParticipant;
--- src/components/Participants/index.js
+++ src/components/Participants/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import OneParticipant from 'src/components/Participants/OneParticipant';
 import { useParams } from 'react-router-dom';
+import Moment from 'react-moment';
 
 import './participants.scss';
 
@@ -17,13 +18,11 @@ const Participants = ({ walks, loading }) => {
           {console.log('rando récupérée', walk.participants)}
           <div className="participants">
             <h2 className="participants_walk_title">Liste des participants inscrits à la randonnée</h2>
-            <h3 className="participants_walk_name">Nom de la randonnée, le 15/04/2021</h3>
+            <h3 className="participants_walk_name">{walk.title}, le <Moment locale="fr" format="dddd DD MMMM YYYY HH:mm">{walk.date}</Moment></h3>
             <div className="participants_list">
               {walk.participants.map((participant) => (
-                // console.log(participant.user)
                 <OneParticipant key={participant.user.id} {...participant.user} />
               ))}
-
             </div>
           </div>
         </>
@@ -33,21 +32,13 @@ const Participants = ({ walks, loading }) => {
 };
 
 Participants.propTypes = {
+  loading: PropTypes.bool.isRequired,
   walks: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
-      startingPoint: PropTypes.string.isRequired,
-      endPoint: PropTypes.string.isRequired,
       date: PropTypes.string.isRequired,
-      difficulty: PropTypes.string.isRequired,
-      duration: PropTypes.string.isRequired,
-      elevation: PropTypes.number.isRequired,
-      maxNbPersons: PropTypes.number.isRequired,
-      area: PropTypes.object.isRequired,
-      description: PropTypes.string.isRequired,
     }).isRequired,
   ).isRequired,
-  loading: PropTypes.bool.isRequired,
 };
 
 export default Participants;
