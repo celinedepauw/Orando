18c028b23832d5b4abb3de3b0af0124388845e6b
Celine Depauw <celinefauchier@hotmail.com>
2021-04-02 15:04:21 +0200

    changes ok

--- src/components/Participants/OneParticipant.js
+++ src/components/Participants/OneParticipant.js
@@ -6,7 +6,7 @@ import photo from 'src/assets/images/Moi.jpg';
 const OneParticipant = ({ id, nickname, creator }) => (
   <div className="oneParticipant">
     <img className="one_participant_image" size={50} src={photo} alt="" />
-    {!creator.id === id ? (<a href={`https://orando.me/o/profile/${id}/contact-user`} className="one_participant_contact">Contacter {nickname}</a>) : (<p className="one_participant_creator">Je suis le seul participant pour le moment</p>)}
+    {id === creator.id ? <p className="one_participant_creator">C'est ma randonnée</p> : <p className="one_participant_contact"><a href={`https://orando.me/o/profile/${id}/contact-user`}>Contacter {nickname}</a></p>}
   </div>
 );
 
