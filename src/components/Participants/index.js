1d68943b00f072b89a060f2f96b7e5785db119c4
Celine Depauw <celinefauchier@hotmail.com>
2021-04-02 13:05:36 +0200

    add the number of participants

--- src/components/Participants/index.js
+++ src/components/Participants/index.js
@@ -15,10 +15,10 @@ const Participants = ({ walks, loading }) => {
       {loading && <div>Chargement en cours...</div>}
       {!loading && (
         <>
-          {console.log('rando récupérée', walk.participants)}
           <div className="participants">
             <h2 className="participants_walk_title">Liste des participants inscrits à la randonnée</h2>
             <h3 className="participants_walk_name">{walk.title}, le <Moment locale="fr" format="dddd DD MMMM YYYY HH:mm">{walk.date}</Moment></h3>
+            <p className="participants_walk_number"> {walk.participants.length} participant(s)</p>
             <div className="participants_list">
               {walk.participants.map((participant) => (
                 <OneParticipant key={participant.user.id} {...participant.user} />
