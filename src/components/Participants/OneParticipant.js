8440dabc1909adb6c3b89e173c4247b9ce28ae83
Celine Depauw <celinefauchier@hotmail.com>
2021-04-03 17:51:50 +0200

    list of participants without the creator of the walk

--- src/components/Participants/OneParticipant.js
+++ src/components/Participants/OneParticipant.js
@@ -3,17 +3,16 @@ import PropTypes from 'prop-types';
 
 import photo from 'src/assets/images/Moi.jpg';
 
-const OneParticipant = ({ id, nickname, creator }) => (
+const OneParticipant = ({ id, nickname }) => (
   <div className="oneParticipant">
     <img className="one_participant_image" size={50} src={photo} alt="" />
-    {id === creator.id ? <p className="one_participant_creator">C'est ma randonnée</p> : <p className="one_participant_contact"><a href={`https://orando.me/o/profile/${id}/contact-user`}>Contacter {nickname}</a></p>}
+    <p className="one_participant_contact"><a href={`https://orando.me/o/profile/${id}/contact-user`}>Contacter {nickname}</a></p>
   </div>
 );
 
 OneParticipant.propTypes = {
   id: PropTypes.number.isRequired,
   nickname: PropTypes.string.isRequired,
-  creator: PropTypes.object.isRequired,
 };
 
 export default OneParticipant;
