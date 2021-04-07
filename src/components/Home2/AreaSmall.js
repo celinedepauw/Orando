2655f4f53dba1c161f4a074deb893c68a382ceae
Celine Depauw <celinefauchier@hotmail.com>
2021-04-07 14:26:11 +0200

    get the colors of areas and display

--- src/components/Home2/AreaSmall.js
+++ src/components/Home2/AreaSmall.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const AreaSmall = ({ id, name, numberOfWalks }) => (
-  <Link to={`/areas/${id}`} className="areasmall">
+const AreaSmall = ({
+  id,
+  name,
+  color,
+  numberOfWalks,
+}) => (
+  <Link to={`/areas/${id}`} className={`areasmall areasmall_${color}`}>
     <h3 className="home_area_name">{name}</h3>
     {numberOfWalks === 0 ? <p className="home_area_noWalk">Pas encore de randonnée...</p> : <p className="home_area_number">{numberOfWalks} randonnée(s)</p>}
   </Link>
@@ -12,6 +17,7 @@ const AreaSmall = ({ id, name, numberOfWalks }) => (
 AreaSmall.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
   numberOfWalks: PropTypes.number.isRequired,
 };
 
