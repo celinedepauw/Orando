6264b6d19479f275ef7d4f54c20421038a80ea50
Alexandrine Heutte <alexandrine.h@gmail.com>
2021-04-07 17:37:12 +0200

    walk to come page ok, have to see with my team if my title is suited

--- src/components/WalksToCome/index.js
+++ src/components/WalksToCome/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import sign from 'src/assets/icones/sign.svg';
+import boot from 'src/assets/icones/boot.svg';
 
 import WalkToComeSmall from 'src/components/WalksToCome/WalkToComeSmall';
 
@@ -16,7 +17,10 @@ const WalksToCome = ({ user, loadingUser, handleCancelParticipation }) => (
         <Link to="/account">
           <img className="walks_to_come_return_account" src={sign} alt="retour" />
         </Link>
-        <h2 className="walks_to_come_title">{user.incomingWalks.length} randonnée(s) à venir</h2>
+        <div className="walks_to_come_title_content">
+          <img className="walks_to_come_icon" src={boot} alt="icone" />
+          <h2 className="walks_to_come_title">{user.incomingWalks.length} randonnée(s) à venir</h2>
+        </div>
         <div className="walks_to_come_list">
           {user.incomingWalks.map((item) => (
             <WalkToComeSmall
