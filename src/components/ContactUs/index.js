31f68f3f3753271bd84aac5714d0a8c2cc792c2b
Celine Depauw <celinefauchier@hotmail.com>
2021-05-10 14:22:21 +0200

    new reducer and container for ContactUs

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,41 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Field from 'src/components/Field';
 import contactLogo from 'src/assets/images/email.png';
 
 import './contactUs.scss';
 
-const ContactUs = () => (
-  <main className="contactUs">
-    <div className="contactUs_title">
-      <img className="contactUs_contact_logo" src={contactLogo} alt="logo-create-walk" />
-      <h2 className="contactUs_title_text">Nous contacter</h2>
-    </div>
-    <form className="contactUs_form">
-      <Field
-        identifier=""
-        placeholder=""
-        label="Sujet"
-        value=""
-        changeField=""
-      />
-      <Field
-        identifier=""
-        placeholder=""
-        label="Email"
-        value=""
-        changeField=""
-      />
-      <Field
-        identifier=""
-        placeholder=""
-        label="Votre message"
-        value=""
-        changeField=""
-      />
-      <button className="contactUs_button" type="submit">Envoyer</button>
-    </form>
-  </main>
-);
+const ContactUs = ({
+  subject,
+  email,
+  message,
+  handleContact,
+  updateContactField,
+}) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleContact();
+  };
+  return (
+    <main className="contactUs">
+      <div className="contactUs_title">
+        <img className="contactUs_contact_logo" src={contactLogo} alt="logo-create-walk" />
+        <h2 className="contactUs_title_text">Nous contacter</h2>
+      </div>
+      <form className="contactUs_form" onSubmit={handleSubmit}>
+        <Field
+          identifier="subject"
+          placeholder=""
+          label="Sujet"
+          value={subject}
+          changeField={(identifier, newValue) => {
+            console.log(`changeField sur sujet : identifier=${identifier}, newValue=${newValue}`);
+            updateContactField(identifier, newValue);
+          }}
+        />
+        <Field
+          identifier="email"
+          placeholder=""
+          label="Email"
+          value={email}
+          changeField={(identifier, newValue) => {
+            console.log(`changeField sur email : identifier=${identifier}, newValue=${newValue}`);
+            updateContactField(identifier, newValue);
+          }}
+        />
+        <Field
+          identifier="message"
+          placeholder=""
+          label="Votre message"
+          value={message}
+          changeField={(identifier, newValue) => {
+            console.log(`changeField sur message : identifier=${identifier}, newValue=${newValue}`);
+            updateContactField(identifier, newValue);
+          }}
+        />
+        <button className="contactUs_button" type="submit">Envoyer</button>
+      </form>
+    </main>
+  );
+};
+
+ContactUs.prototypes = {
+  subject: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  handleContact: PropTypes.func.isRequired,
+  updateContactField: PropTypes.func.isRequired,
+};
 
 export default ContactUs;
